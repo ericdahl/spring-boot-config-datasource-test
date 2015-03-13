d42768af04c16cb5923410ed6e7309b5217eb8fd
Eric Dahl <eric.c.dahl@gmail.com>
2015-03-13 15:59:35 -0700

    initial

---
+++ src/main/java/example/DataSourceConfig.java
@@ -0,0 +1,34 @@
+package example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataSourceConfig.class);
+
+    @RefreshScope
+    @Bean
+    public DataSource customDataSource() {
+        final String url = "jdbc:h2:mem:customDb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE";
+
+        LOGGER.info("Creating in-memory h2 database with jdbc url [{}]", url);
+        return DataSourceBuilder
+                .create()
+                .driverClassName(EmbeddedDatabaseConnection.H2.getDriverClassName())
+                .url(url)
+                .username("sa")
+                .password("")
+                .build();
+
+    }
+}
